a74e9c798d8132655f008ed85a6a26ce4dcd320e
okayzhr <okayzhr@gmail.com>
2024-12-27 11:39:35 +0100

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/copy-from/414863112
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
